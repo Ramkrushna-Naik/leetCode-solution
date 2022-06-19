b9a7b2a1478751879cf08f99bc52960acdcbb46c
Ramkrushna Naik <63165815+Ramkrushna-Naik@users.noreply.github.com>
2022-06-20 03:28:22 +0530

    Time: 172 ms (23.52%), Space: 14.2 MB (67.93%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -3,7 +3,9 @@ class Solution:
         n=nums1[:]+nums2[:]
         n.sort()
         if(len(n)%2==0):
-            return ((n[int(len(n)/2)-1]+n[int(len(n)/2)])/2)
+            a=n[int(len(n)/2)-1]
+            b=n[int(len(n)/2)]
+            return (a+b)/2
         else:
             return n[int(len(n)/2)]
         
